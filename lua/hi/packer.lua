d7fea3304a18310e627c2184932181971947b86d
park102 <BJohn316@outlook.com>
2023-04-03 18:51:50 -0400

    a

--- lua/hi/packer.lua
+++ lua/hi/packer.lua
@@ -46,6 +46,11 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 	  }
   }
+  use {
+  'lewis6991/gitsigns.nvim',
+  -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+}
+  use("tpope/vim-fugitive")
   use('nvim-treesitter/nvim-treesitter-context')
 end)
 
