5498abc37e7b4ef5ee86effdec2dc27724b2679e
park102 <BJohn316@outlook.com>
2023-04-16 21:20:40 -0400

    keybinds

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>fb', builtin.git_branches, {})
