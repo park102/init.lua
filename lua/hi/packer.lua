40dbcdcdfd196f807e819fb9e2d3f023f60b7b78
park102 <BJohn316@outlook.com>
2023-05-14 16:42:07 -0400

    added nvim-tree

--- lua/hi/packer.lua
+++ lua/hi/packer.lua
@@ -4,9 +4,10 @@
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'nvim-tree/nvim-web-devicons'
-  use 'feline-nvim/feline.nvim'
-  use 'wbthomason/packer.nvim'
+  use('nvim-tree/nvim-tree.lua')
+  use('nvim-tree/nvim-web-devicons')
+  use('feline-nvim/feline.nvim')
+  use('wbthomason/packer.nvim')
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.0',
 -- or                            , branch = '0.1.x',
--- lua/hi/remap.lua
+++ lua/hi/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>j",vim.cmd.Ex)
+vim.keymap.set("n","<leader>j","<cmd>NvimTreeToggle<CR>")
 vim.keymap.set("i","<C-c>","<Esc>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
